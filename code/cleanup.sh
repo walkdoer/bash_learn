459df517f5dc0c03fcd2c390daef7671062f3289
zhangmh <zhangmhao@gmail.com>
2014-03-19 10:56:37 +0800

    [update] cleanup.sh

--- code/cleanup.sh
+++ code/cleanup.sh
@@ -2,13 +2,37 @@
 
 
 LOG_DIR=/var/log
+ROOT_UID=0  #$UID为0，用户才具有Root权限
+LINES=50 #默认保存行数
+E_XCD=66 #不能修改目录
+E_NOTROOT=67 #不是root用户
+
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+    echo "Must be root to run this script."
+    exit $E_NOTROOT
+fi
+
+if [ -n "$1" ]
+then
+    lines=$1
+else
+    lines=$LINES
+fi
+
 
 cd $LOG_DIR
+if [ `pwd` != "$LOG_DIR" ]
+then
+    echo "Can't change to $LOG_DIR"
+    exit $E_XCD
+fi
 
-
-cat /dev/null > messages
-cat /dev/null > wtmp
+tail -$lines message > mesg.temp #保存logfile
+mv mesg.temp message
+#cat /dev/null > message
+#cat /dev/null > wtmp
 
 echo "Logs cleaned up."
 
-exit #退出脚本
+exit 0 #返回0退出表示成功
