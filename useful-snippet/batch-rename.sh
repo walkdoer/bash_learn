cd253c77d04fee01bb235b2353c310e0b726a992
zhangmh <zhangmhao@gmail.com>
2014-05-19 09:53:33 +0800

    添加reference

--- useful-snippet/batch-rename.sh
+++ useful-snippet/batch-rename.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#Reference:http://www.peteryu.ca/tutorials/shellscripting/batch_rename
 
 # 将 test.xxx!mid 之类的文件名重命名为 test.xxx
 for i in *mid; do mv "$i" "${i/\!mid/}"; done;
 
 #todo
 # 将该脚本扩展为可以传参数的执行的脚本
+
+
